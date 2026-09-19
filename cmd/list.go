de7f3c2f090ebe49c5a94077dd8b48fec4e23d87
Abhijit <abhijitkumar1859@gmail.com>
2026-09-19 19:54:53 +0530

    general commands added

--- cmd/list.go
+++ cmd/list.go
@@ -1,1 +1,52 @@
 package cmd
+
+import (
+	"context"
+	"fmt"
+ 
+	"github.com/abhijit1859/eternal_grimoire/internal/storage"
+	"github.com/spf13/cobra"
+)
+
+
+
+var listCmd=&cobra.Command{
+	Use: "List backups",
+	Short: "List all available backups in the storage backend",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		ctx:=cmd.Context()
+		if ctx==nil{
+			ctx=context.Background()
+		}
+
+		backupDir:="backups"
+
+		if cfg.Storage.Path!=""{
+			backupDir=cfg.Storage.Path
+		}
+
+		store:=storage.NewLocalStorage(backupDir)
+
+		backups,err:=store.List(ctx)
+		if err!=nil{
+			return fmt.Errorf("failed to list backups because of error: %w",err)
+
+		}
+
+		if len(backups)==0{
+			fmt.Printf("No backups found.")
+			return nil
+		}
+
+		fmt.Printf("Found %d backup(s) in %s : \n",len(backups),backupDir)
+
+		for i,name:=range backups{
+			fmt.Printf("[%d] %s\n",i+1,name)
+		}
+		return nil
+	},
+}
+
+func init(){
+	rootCmd.AddCommand(listCmd)
+}
--- cmd/restore.go
+++ cmd/restore.go
@@ -1,3 +1,72 @@
 package cmd
 
- 
+import (
+	"context"
+	"fmt"
+	"io"
+	"time"
+
+	"github.com/abhijit1859/eternal_grimoire/internal/backup"
+	"github.com/abhijit1859/eternal_grimoire/internal/compression"
+	 
+	"github.com/abhijit1859/eternal_grimoire/internal/storage"
+	"github.com/spf13/cobra"
+)
+
+var restorefile string
+
+var restoreCmd = &cobra.Command{
+	Use:   "Restore Database",
+	Short: "",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		ctx := cmd.Context()
+		if ctx == nil {
+			ctx = context.Background()
+		}
+
+		startTime := time.Now()
+
+		engine := backup.NewPostgresBackup(cfg.Database)
+		compressor := compression.NewGzipCompressor()
+		backupDir := "backups"
+
+		if cfg.Storage.Path != "" {
+			backupDir = cfg.Storage.Path
+		}
+
+		store := storage.NewLocalStorage(backupDir)
+
+		fmt.Printf("Starting to restore database: %s from archive: %s: ...\n", cfg.Database.Database, restorefile)
+
+		archiveStream, err := store.Retrieve(ctx, restorefile)
+
+		if err != nil {
+			fmt.Printf("Failed to restore database %w", err)
+		}
+		defer archiveStream.Close()
+
+		restoreR, restoreW := io.Pipe()
+
+		go func() {
+			defer restoreW.Close()
+			if err := compressor.Decompress(archiveStream, restoreW); err != nil {
+				restoreW.CloseWithError(fmt.Errorf("decompression failed: %w", err))
+			}
+		}()
+
+		if err := engine.Restore(ctx, restoreR); err != nil {
+			return fmt.Errorf("Database restore failed: %w", err)
+		}
+		fmt.Printf("Database successfully restored Duration %s \n", time.Since(startTime).Round(time.Millisecond))
+
+		return nil
+
+	},
+}
+
+func init(){
+	restoreCmd.Flags().StringVarP(&restorefile,"file","f","","Backup filename to restore (required)")
+
+	restoreCmd.MarkFlagRequired("file")
+	rootCmd.AddCommand(restoreCmd)
+}
